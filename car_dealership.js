b071faff03fcda3d8523646213e0e731e2533d2a
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-22 21:32:28 +0100

    prototypes for dealership

--- car_dealership.js
+++ car_dealership.js
@@ -19,10 +19,21 @@ this.capacity = capacity;
 this.stock = [];
 
 Dealership.prototype.getName = function() { return this.name};
-Dealership.prototype.setName = function(manufacturer) { this.name = this.name };
+Dealership.prototype.setName = function(name) { this.name = this.name };
+Dealership.prototype.getCapacity = function() { return this.capacity};
+Dealership.prototype.setCapacity = function(capacity) { this.capacity = this.capacity };
+Dealership.prototype.getName = function() { return this.name};
+Dealership.prototype.setName = function(name) { this.name = this.name };
+Dealership.prototype.getStock = function() { return this.stock};
+Dealership.prototype.setStock = function(stock) { this.stock = this.stock};
 
-Dealership.prototype.addCar = function () { return stock.push(Car)};
-Dealership.prototype.countStock = function() {return stock.length};
+Dealership.prototype.addCar = function (newCar) {
+    this.Car = newCar;
+    return stock.push(Car)};
+
+Dealership.prototype.countStock = function() {
+    return stock.length};
+
 this.getCourses = () => _courses;
 }
 
