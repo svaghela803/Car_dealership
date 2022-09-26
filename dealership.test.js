38d307f9340ad3f830c66a66cc82ecdb99c8679f
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-26 20:04:07 +0100

    test for dealership add car

--- dealership.test.js
+++ dealership.test.js
@@ -1,4 +1,6 @@
-test ("Dealership - count()", () = {
+
+
+test("Dealership - count()", () => {
     let name = "Test D";
     let maxNumberOfCars = 10;
     let cars = [new Car("", 100, ""), new Car("", 100, "")];
@@ -7,4 +9,19 @@ test ("Dealership - count()", () = {
     dealership.cars = cars;
 
     expect(dealership.count()).toBe(2);
-});
+})
+
+test("Dealership - add() - success", () => {
+    let name = "Test D";
+    let maxNumberOfCars = 3;
+    let cars = [new Car("", 100, ""), new Car("", 100, "")];
+
+    let dealership = new Dealership(name, maxNumberOfCars);
+    dealership.cars = cars;
+    expect(dealership.count()).toBe(2);
+
+    dealership.add(new Car("", 100, ""));
+    expect(dealership.count()).toBe(3);
+})
+
+
